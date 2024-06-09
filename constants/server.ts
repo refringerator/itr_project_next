74dd2cc225262cc18e8359c4af6402ae10df6107
petruha <refloader@gmail.com>
2024-06-09 03:37:13 +0300

    Add help center link

--- constants/server.ts
+++ constants/server.ts
@@ -2,6 +2,6 @@ import "server-only";
 
 export const defaultImage = `/acorn.jpg`;
 
-export const jiraUrl = process.env.JIRA_URL!;
+export const jiraUrl = process.env.NEXT_PUBLIC_JIRA_URL!;
 export const jiraAuthHeader = process.env.JIRA_AUTH_HEADER!;
 export const jiraServicedeskId = "1";
--- sections/Layout/Footer.tsx
+++ sections/Layout/Footer.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { Layout } from "antd";
+import { jiraHelpCenter } from "@/constants";
+import { Divider, Layout } from "antd";
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 const Footer = () => {
   const t = useTranslations("Footer");
 
   return (
     <Layout.Footer style={{ textAlign: "center" }}>
-      {t("created_by")} &copy;petruha
+      {t("created_by")} &copy;petruha <Divider type="vertical" />
+      <Link href={jiraHelpCenter}>Help Center</Link>
     </Layout.Footer>
   );
 };
