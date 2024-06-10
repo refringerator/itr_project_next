7bdb9441fa17c06eb2647ceaa6896e00855c2608
petruha <refloader@gmail.com>
2024-06-10 04:21:13 +0300

    Add HelpCenterLink component

--- components/UserHelper.tsx
+++ components/UserHelper.tsx
@@ -1,48 +1,88 @@
 "use client";
 
-import { FloatButton, Modal } from "antd";
+import { FloatButton, Modal, Typography } from "antd";
+import { Button, message, Space } from "antd";
 import {
   QuestionCircleOutlined,
   ExclamationCircleFilled,
 } from "@ant-design/icons";
 import { createCustomerRequest } from "@/app/[locale]/actions";
 import { Context } from "@/context";
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
+import { HelpCenterLink } from "./HelpCenterLink";
+import Link from "next/link";
 
 const { confirm } = Modal;
 
-const showPromiseConfirm = (userId: string, userEmail: string) => {
-  confirm({
-    title: "Do you want to delete these items?",
-    icon: <ExclamationCircleFilled />,
-    content:
-      "When clicked the OK button, this dialog will be closed after 1 second",
-    async onOk() {
-      try {
-        return await createCustomerRequest(userId, userEmail);
-        // return await new Promise((resolve, reject) => {
-        //   setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
-        // });
-      } catch {
-        return console.log("Oops errors!");
-      }
-    },
-    onCancel() {},
-  });
-};
-
 export default function UserHelper() {
   const context = useContext(Context);
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const showPromiseConfirm = useCallback(
+    (userId: string, userEmail: string) => {
+      console.log({ userEmail, userId });
+
+      const content =
+        userId === "" ? (
+          <>
+            <p>
+              You need <Link href="/signin">login</Link> first to ask question
+              in Collections App
+            </p>
+            <p>
+              <Typography.Text>
+                Or you can create request on our{" "}
+              </Typography.Text>
+              <HelpCenterLink />
+            </p>
+          </>
+        ) : (
+          "When clicked the OK button, this dialog will be closed after 1 second"
+        );
+      confirm({
+        title: "Need help?",
+        icon: <ExclamationCircleFilled />,
+        content,
+
+        async onOk() {
+          try {
+            await createCustomerRequest(userId, userEmail);
+            // return await new Promise((resolve, reject) => {
+            //   setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
+            // });
+            messageApi.open({
+              type: "success",
+              content: "New request created!",
+            });
+          } catch {
+            messageApi.open({
+              type: "error",
+              content: "This is an error",
+            });
+            return console.log("Oops errors!");
+          }
+        },
+        onCancel() {},
+      });
+    },
+    [messageApi]
+  );
 
   return (
-    <FloatButton
-      icon={<QuestionCircleOutlined />}
-      type="default"
-      style={{ right: 24 }}
-      tooltip={<div>Need help?</div>}
-      onClick={() =>
-        showPromiseConfirm(context?.user?.id || "", context?.user?.email || "")
-      }
-    />
+    <>
+      {contextHolder}
+      <FloatButton
+        icon={<QuestionCircleOutlined />}
+        type="default"
+        style={{ right: 24 }}
+        tooltip={<div>Need help?</div>}
+        onClick={() =>
+          showPromiseConfirm(
+            context?.user?.id || "",
+            context?.user?.email || ""
+          )
+        }
+      />
+    </>
   );
 }
--- sections/Layout/Footer.tsx
+++ sections/Layout/Footer.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { jiraHelpCenter } from "@/constants";
+import { HelpCenterLink } from "@/components/HelpCenterLink";
 import { Divider, Layout } from "antd";
 import { useTranslations } from "next-intl";
-import Link from "next/link";
 
 const Footer = () => {
   const t = useTranslations("Footer");
@@ -11,7 +10,7 @@ const Footer = () => {
   return (
     <Layout.Footer style={{ textAlign: "center" }}>
       {t("created_by")} &copy;petruha <Divider type="vertical" />
-      <Link href={jiraHelpCenter}>Help Center</Link>
+      <HelpCenterLink />
     </Layout.Footer>
   );
 };
