ff8d4e6ce1c426a1705181068e4b55ee90137802
petruha <refloader@gmail.com>
2024-06-10 20:17:04 +0300

    Add token generation

--- components/UserToken.tsx
+++ components/UserToken.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { getToken } from "@/app/[locale]/account/actions";
+import { generateToken, getToken } from "@/app/[locale]/account/actions";
 import { Context } from "@/context";
-import { Typography, Button } from "antd";
+import { Typography } from "antd";
 import { useContext, useEffect, useState } from "react";
 
 export default function UserToken() {
   const [token, setToken] = useState("");
+  const [loading, setLoading] = useState(false);
   const context = useContext(Context);
 
   useEffect(() => {
@@ -17,10 +18,28 @@ export default function UserToken() {
     d();
   }, [context?.user?.id]);
 
+  const onClick = async () => {
+    setLoading(true);
+    const token = await generateToken(context?.user?.id || null);
+    setToken(token);
+    setLoading(false);
+  };
+
   return (
-    <p>
-      <Typography.Text>Your API token: </Typography.Text>
-      <Typography.Text copyable>{token}</Typography.Text>
-    </p>
+    <>
+      {token !== "" && (
+        <p>
+          <Typography.Text>Your API token: </Typography.Text>
+          <Typography.Text copyable>{token}</Typography.Text>
+        </p>
+      )}
+      <p>
+        <Typography.Text>You can </Typography.Text>
+        <Typography.Link disabled={loading} onClick={onClick}>
+          generate {token !== "" && "new"}
+        </Typography.Link>
+        <Typography.Text> API token</Typography.Text>
+      </p>
+    </>
   );
 }
